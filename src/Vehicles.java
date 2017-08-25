0f048b8104c7b8cae95df20d72886d4c0c086646
rtomil <rtomilobil@gmail.com>
2017-08-25 11:53:09 +0200

    change name from CreateVehicles to Vehicles, add Motorcycle method

--- src/Vehicles.java
+++ src/Vehicles.java
@@ -1,6 +1,36 @@
-public class CreateVehicles {
+public class Vehicles {
+
+    Car[] carList;
+    Motorcycle[] bikeList;
 
     public void Cars(){
-
+        Car car1 = new Car();
+        Car car2 = new Car();
+        Car car3 = new Car();
+        Car car4 = new Car();
+        Car car5 = new Car();
+        Car car6 = new Car();
+        Car car7 = new Car();
+        Car car8 = new Car();
+        Car car9 = new Car();
+        Car car10 = new Car();
+        Car[] carList = new Car[]{car1, car2, car3, car4, car5, car6, car7, car8, car9, car10};
+        this.carList = carList;
     }
+
+    public void Motorcycles(){
+        Motorcycle bike1 = new Motorcycle();
+        Motorcycle bike2 = new Motorcycle();
+        Motorcycle bike3 = new Motorcycle();
+        Motorcycle bike4 = new Motorcycle();
+        Motorcycle bike5 = new Motorcycle();
+        Motorcycle bike6 = new Motorcycle();
+        Motorcycle bike7 = new Motorcycle();
+        Motorcycle bike8 = new Motorcycle();
+        Motorcycle bike9 = new Motorcycle();
+        Motorcycle bike10 = new Motorcycle();
+        Motorcycle[] bikeList = new Motorcycle[]{bike1, bike2, bike3, bike4, bike5, bike6, bike7, bike8, bike9, bike10};
+        this.bikeList = bikeList;
+    }
+
 }
