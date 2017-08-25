f91ac9fa97dd1f31717e93fe9472e54ab48fcf29
rtomil <rtomilobil@gmail.com>
2017-08-25 11:23:31 +0200

    init Main

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+/*
+static isRaining //30% chance of rain every hour
+ createVehicles() // creates 10 cars, 10 trucks and 10 motorcycles
+ simulateRace() // simulates the race by calling moveForAnHour() on every vehicle 50 times and setting whether its raining.
+ printRaceResults() // prints each vehicle's name, distance traveled ant type.
+ */
+
+public class Main {
+    static boolean isRaining;
+
+    public static void main(String[] args){
+        CreateVehicles vehicles = new CreateVehicles();
+    }
+
+    }
